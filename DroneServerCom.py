2e40d4a0159117eb8d86125c7a541abfc041e8fe
Timothy Scott <timjimski@gmail.com>
2018-04-19 18:19:43 -0400

    Added POST request code server to drone

--- DroneServerCom.py
+++ DroneServerCom.py
@@ -3,17 +3,19 @@ import time
 import os
 import GPSWrapper
 #Specifies where to send data to
-API_DESTINATION = "https://deliverwithatlas.com/UpdateDroneStatus.php"
+API_DESTINATION_TO_SERVER = "https://deliverwithatlas.com/UpdateDroneStatus.php"
+API_DESTINATION_FROM_SERVER = "https://deliverwithatlas.com/UpdateWaypoint.php"
 
 #Creates a gps object that holds all values to be sent to server
 #Communicates via threads to constantly send new coordinates every second
 gps = GPSWrapper.thread
 
 #Set default values for aircraft being used
-droneID = 55560935
+droneID = 1234
 dronePrivateKey = 1234567890
 currentBatteryLife = 100
 currentStageOfDelivery = 'idle'	#Needs to be updated
+isBlocked = False #Needs to be updated constantly based on sensor readings
 try:
 	gps.start()
 	while True:
@@ -25,11 +27,19 @@ try:
         	      	 "current_stage_of_delivery":currentStageOfDelivery, "latitude":latitude, "longitude":longitude,
               		 "altitude":altitude, "speed":speed}
 
-		response = requests.post(url = API_DESTINATION, data = droneStatus)
+		response = requests.post(url = API_DESTINATION_TO_SERVER, data = droneStatus)
 
 		#For testing
-		print("The result from sending data to server was: %s", response.text)
+		#print("The result from sending data to server was: ", response.text)
+
+		obstacleStatus = {"drone_id":droneID, "drone_private_key":dronePrivateKey, "is_blocked":isBlocked}
+		
+		response = requests.post(url = API_DESTINATION_FROM_SERVER, data = obstacleStatus)
+
+		#For testing
+		#print("Result from getting new waypoint from server: ", response.text)
 		time.sleep(1)
+
 except (KeyboardInterrupt, SystemExit): 
 	print("\nKilling Thread...")
 	gps.running = False
